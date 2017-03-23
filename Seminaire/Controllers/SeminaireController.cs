3e121e6b24857a42cd3826d44ef2960f6c0a48ca
Eric Cote <eric@coteexpert.com>
2017-03-23 14:40:04 -0400

    Carousel

--- Seminaire/Controllers/SeminaireController.cs
+++ Seminaire/Controllers/SeminaireController.cs
@@ -7,85 +7,95 @@ using System.Web.Mvc;
 
 namespace Seminaire.Controllers
 {
-    public class SeminaireController : Controller
+  public class SeminaireController : Controller
+  {
+    // GET: Seminaire
+    public ActionResult Index()
     {
-        // GET: Seminaire
-        public ActionResult Index()
-        {
-            SeminaireContext db = new SeminaireContext();
+      SeminaireContext db = new SeminaireContext();
 
-            IEnumerable<Conference> liste = db.Conferences;
-            return View(liste);
-        }
-
-        // GET: Seminaire/details/5
-        public ActionResult Details(int id)
-        {
-            //1.vérifier que le séminaire existe.
-            //2.aller chercher le seminaire
-
-            SeminaireContext db = new SeminaireContext();
-            Conference conf =   db.Conferences.Find(id);
-
-            if (conf == null)
-            {
-                return HttpNotFound();
-            }
-            else
-            {
-                return View(conf);
-            }
-        }
-
-
-        //POST: Seminaire/Create/5
-        [HttpPost()]
-        public ActionResult Create(Participant part, int id)
-        {
-            SeminaireContext db = new SeminaireContext();
-
-            if (!ModelState.IsValid)
-            {
-               return View("CreateFail");
-            }
-            Conference conf = db.Conferences.Find(id);
-            if (conf == null)
-            {
-                return HttpNotFound();
-            }
-            else
-            {
-                part.Conferences.Add(conf);
-                db.Participants.Add(part);
-                db.SaveChanges();
-                return View("CreateSucces");
-
-            }
-               
-           
-
-        }
-
-
-
-        public ActionResult Inscriptions(int id)
-        {
-            SeminaireContext db = new SeminaireContext();
-            Conference conf = db.Conferences.Find(id);
-            if (conf == null)
-            {
-                return HttpNotFound();
-            }
-            else
-            {
-                return View(conf.Participants);
-            }
-        }
-
-
-        public ActionResult Add(int i, int j)
-        {
-            return View(i + j);
-        }
+      IEnumerable<Conference> liste = db.Conferences;
+      return View(liste);
     }
+
+
+    public PartialViewResult _GetCarousel()
+    {
+      SeminaireContext db = new SeminaireContext();
+
+      IEnumerable<Conference> liste = db.Conferences;
+
+      return PartialView("_SeminaireCarousel",liste);
+    }
+
+    // GET: Seminaire/details/5
+    public ActionResult Details(int id)
+    {
+      //1.vérifier que le séminaire existe.
+      //2.aller chercher le seminaire
+
+      SeminaireContext db = new SeminaireContext();
+      Conference conf = db.Conferences.Find(id);
+
+      if (conf == null)
+      {
+        return HttpNotFound();
+      }
+      else
+      {
+        return View(conf);
+      }
+    }
+
+
+    //POST: Seminaire/Create/5
+    [HttpPost()]
+    public ActionResult Create(Participant part, int id)
+    {
+      SeminaireContext db = new SeminaireContext();
+
+      if (!ModelState.IsValid)
+      {
+        return View("CreateFail");
+      }
+      Conference conf = db.Conferences.Find(id);
+      if (conf == null)
+      {
+        return HttpNotFound();
+      }
+      else
+      {
+        part.Conferences.Add(conf);
+        db.Participants.Add(part);
+        db.SaveChanges();
+        return View("CreateSucces");
+
+      }
+
+
+
+    }
+
+
+
+    public ActionResult Inscriptions(int id)
+    {
+      SeminaireContext db = new SeminaireContext();
+      Conference conf = db.Conferences.Find(id);
+      if (conf == null)
+      {
+        return HttpNotFound();
+      }
+      else
+      {
+        return View(conf.Participants);
+      }
+    }
+
+
+    public ActionResult Add(int i, int j)
+    {
+      return View(i + j);
+    }
+  }
 }
